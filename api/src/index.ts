3142bc18e499859640c5351e8ddb2349236e6fb3
darikutelov <dari@kutelov.com>
2024-10-27 20:06:39 +0200

    Orders

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,9 @@
 import express, { json, urlencoded } from "express"
+import serverless from "serverless-http"
+
 import productRouter from "./routes/products/index.js"
 import authRouter from "./routes/auth/index.js"
+import orderRouter from "./routes/orders/index.js"
 
 const port = 8000
 const app = express()
@@ -9,7 +12,12 @@ app.use(json())
 
 app.use("/products", productRouter)
 app.use("/auth", authRouter)
+app.use("/orders", orderRouter)
 
-app.listen(port, () => {
-  console.log(`🎯 Ecommerce api listening on port: ${port}!`)
-})
+if (process.env.NODE_ENV == "dev") {
+  app.listen(port, () => {
+    console.log(`🎯 Ecommerce api listening on port: ${port}!`)
+  })
+}
+
+export const handler = serverless(app)
